bc40df09971159a9c6f3120aad35fc558e41dc37
Alvaro Gomez <arananegrayeye@gmail.com>
2017-03-27 18:38:26 +0200

    Primera opción de uso de pymongo (continuar investigando mongoengine). El dominio será común para ambos módulos {id, secuencia}. En la bs de NCBIretriever, se harán las llamadas al servicio del NCBI para recuperar las secuencias requeridas a partir de distintos formatos. En la bs de subsequenceMatcher, se recogerán los datos de mongodb de la colección donde se guardan las secuencias y se harán limpiezas según la subsecuencia especificada.

--- bioSpark/common/domain.py
+++ bioSpark/common/domain.py
@@ -1,17 +1,21 @@
 import re
 from bioSpark.common.util import Constants
 
+import mongoengine
+
+# option 1
+
 class NucleotidesFromNCBI(object):
     """NucleotidesFromNCBI model"""
-    def __init__(self, id = '0', sequence = '_'):
-        self.id = id
+    def __init__(self, idNcbi ='0', sequence ='_'):
+        self.idNcbi = idNcbi
         self.sequence = sequence
 
-    def get_id(self) -> str:
-        return self._id
+    def get_idNcbi(self) -> str:
+        return self._idNcbi
 
-    def set_id(self, id) -> None:
-        self._id = id
+    def set_idNcbi(self, id) -> None:
+        self._idNcbi = id
 
     def get_sequence(self) -> str:
         return self._sequence
@@ -23,12 +27,14 @@ class NucleotidesFromNCBI(object):
             raise ValueError(Constants.MSG_ERROR_DNA_SEQUENCE)
         self._sequence = sequence
 
-    id = property(fget= get_id, fset= set_id)
+    idNcbi = property(fget= get_idNcbi, fset= set_idNcbi)
     sequence = property(fget= get_sequence, fset= set_sequence)
 
-# test = NucleotidesFromNCBI(id= "01", sequence="GCATCA")
-#
-# print(test.sequence)
+# option 2
+
+test = NucleotidesFromNCBI(idNcbi= "01", sequence="GCATCA")
+
+print(test.idNcbi)
 
 
 
--- testing.py
+++ testing.py
@@ -1,14 +1,34 @@
 # script for testing
 
-# bs 
-from mongoengine import *
+from pymongo import MongoClient
 
-connect('SparkTest', host='localhost', port=27017)
+from bioSpark.common import domain
+
+client = MongoClient('localhost', 27017)
+
+db = client.SparkTestDB
+sequencesCollection = db.sequences
+
+test = domain.NucleotidesFromNCBI(idNcbi="002", sequence="ACGACT")
+
+# If the document does not specify an _id field, then MongoDB will add the _id field
+# and assign a unique ObjectId for the document before inserting.
+# Most drivers create an ObjectId and insert the _id field, but
+# the mongod will create and populate the _id if the driver or application does not.
+
+print(test.__dict__)
+
+sequencesCollection.insert(test.__dict__)
 
 
-class Sequences(Document):
-    idNcbi = StringField(required=True)
-    sequence = StringField()
 
-#Sequences(idNcbi='01', sequence = 'CTGAGCAT').save()
-#Sequences(idNcbi='02', sequence = 'CGTAGCTAG').save()
+
+
+# class MongoCon(object):
+#     __db = None
+#
+#     @classmethod
+#     def get_connection(client):
+#         if client.__db is None:
+#             client.__db = Connection()
+#         return client.__db
