1af7c10137922340382bbeb712e7d4b5d2ff8282
Alvaro Gomez <arananegrayeye@gmail.com>
2017-03-30 20:42:33 +0200

    Estilo de implementación elegida para mongo y modelo de dominio de secuencias del ncbi. (Caso de prueba es testing.py)

--- testing.py
+++ testing.py
@@ -1,31 +1,11 @@
 # script for testing
-from bioSpark.common.domain import NucleotidesFromNCBIusingIdOfSequenceInMongo
-from bioSpark.common.pymongoConnection import dc
+from bioSpark.common.domain import NucleotidesFromNCBI as ncbiNucleotides
 import bioSpark.common.pymongoSimpleConnection as mongoConnect
 
-test = NucleotidesFromNCBIusingIdOfSequenceInMongo(id="001", sequence="ACGACT")
-test2 = NucleotidesFromNCBIusingIdOfSequenceInMongo(id="002", sequence="ACGACT")
+test = ncbiNucleotides(idNcbi = "001", sequence="ACGACT")
+test2 = ncbiNucleotides(idNcbi = "002", sequence="GTCAGCATG")
 
-
-# using the 'singleton' pattern with classes --> pymongoConnection
-
-# If the document does not specify an _id field, then MongoDB will add the _id field
-# and assign a unique ObjectId for the document before inserting.
-# Most drivers create an ObjectId and insert the _id field, but
-# the mongod will create and populate the _id if the driver or application does not.
-
-# create connection
-dc.connect('localhost', 27017)
-dc.connect()
-
-
-# using just modules --> pymongoSimpleConnection 
-
-# from that connection, create (or use) collection and perform operations
-
-dc.sequencesTest.insert(test.__dict__)
-dc.test01.insert(test.__dict__)
-dc.sequencesTest.insert(test2.__dict__)
 
 sequencesCollection = mongoConnect.getCollectionFromDb(collectionName="test01", mongoHost='localhost',
                                                        connectionPort=27017, mongoDb="SparkTest")
+sequencesCollection.insert(test.__dict__)
