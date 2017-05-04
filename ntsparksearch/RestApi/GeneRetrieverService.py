8db18164ef1ed4f057f57b38a3d38a9d45386eef
Alvaro Gomez <arananegrayeye@gmail.com>
2017-05-05 00:31:02 +0200

    Añadido método de prueba con abort para mensajes de error.

--- ntsparksearch/RestApi/GeneRetrieverService.py
+++ ntsparksearch/RestApi/GeneRetrieverService.py
@@ -1,17 +1,18 @@
 import os
 import json
-from flask import Blueprint, request,Response, redirect, url_for, send_from_directory, jsonify
+from flask import Blueprint, request, Response, redirect, url_for, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 from ntsparksearch.GeneRetriever.GeneRetrieverBS import GeneRetrieverBS
 from ntsparksearch.Common.Constants import Constants
 
-
 GeneRetrieverService_endpoints = Blueprint('GeneRetrieverService', __name__)
+
 
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in Constants.ALLOWED_EXTENSIONS
+
 
 # Route that will process the file upload
 @GeneRetrieverService_endpoints.route('/upload_file', methods=['POST', 'GET'])
@@ -28,7 +29,7 @@ def upload_file():
             file.save(os.path.join(Constants.UPLOAD_FOLDER, filename))
             # Redirect the user to the uploaded_file route, which
             # will basicaly show on the browser the uploaded file
-            #return redirect(url_for('uploaded_file',
+            # return redirect(url_for('uploaded_file',
             #                   filename=filename))
             return "file uploaded succesfully"
     return '''
@@ -41,20 +42,22 @@ def upload_file():
     </form>
     '''
 
+
 @GeneRetrieverService_endpoints.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(Constants.UPLOAD_FOLDER,
                                filename)
 
+
 @GeneRetrieverService_endpoints.route('/ounfiltered', methods=['GET'])
 def obtainUnfiltered():
     retriever_BS = GeneRetrieverBS()
     list_of_genes = retriever_BS.get_list_of_ids_from_mongo()
-    return Response(json.dumps(list_of_genes),  mimetype='application/json')
+    return Response(json.dumps(list_of_genes), mimetype='application/json')
 
-# if __name__ == '__main__':
-#     app.run(
-#         #host="0.0.0.0",
-#         port=int("5000"),
-#         debug=True
-#     )
+    # if __name__ == '__main__':
+    #     app.run(
+    #         #host="0.0.0.0",
+    #         port=int("5000"),
+    #         debug=True
+    #     )
--- ntsparksearch/RestApi/SubSequenceMatcherService.py
+++ ntsparksearch/RestApi/SubSequenceMatcherService.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, abort
 
 SubSequenceMatcherService_endpoints = Blueprint('SubSequenceMatcherService', __name__)
 
 @SubSequenceMatcherService_endpoints.route("/account")
 def accountList():
     return "list of accounts"
+
+
+@SubSequenceMatcherService_endpoints.route("/failed")
+def test():
+    return abort(401)
