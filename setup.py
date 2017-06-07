95e20dfa82e24ed37ccc6f40547caa66b6f6542f
Alvaro Gomez <arananegrayeye@gmail.com>
2017-06-07 18:49:12 +0200

    Modified description of setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Nucleotide Spark Exact Searcher',
       version='1.0.0',
       description='This application is the final degree project of Álvaro Gómez Jáuregui. It uses Spark to'
-                  'perform exact searching through a set of genes imported from excel files. This program also'
+                  'perform exact searching through a set of genes imported from excel or fasta files. This program also'
                   'performs the download of the sequences from the NCBI and management of a mongodb database'
                   'to store the data',
       author='Álvaro Gómez Jáuregui',
