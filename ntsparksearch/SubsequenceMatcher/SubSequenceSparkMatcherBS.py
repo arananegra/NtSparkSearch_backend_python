a146513721bf5f157551c684724d7276eaa595f4
Alvaro Gomez <arananegrayeye@gmail.com>
2017-07-08 23:01:35 +0200

    Cluster mode added

--- ntsparksearch/SubsequenceMatcher/SubSequenceSparkMatcherBS.py
+++ ntsparksearch/SubsequenceMatcher/SubSequenceSparkMatcherBS.py
@@ -21,7 +21,7 @@ class SubSequenceSparkMatcherBS(ISubSequenceSparkMatcher):
             return len(SeqUtils.nt_search(x[1], subsequence)) > 1
 
         try:
-            conf = SparkConf().setAppName("ntsparksearch").setMaster("local[8]")
+            conf = SparkConf().setAppName("ntsparksearch").setMaster("spark://172.16.239.10:7077")
 
             sc = SparkContext(conf=conf)
 
@@ -50,7 +50,7 @@ class SubSequenceSparkMatcherBS(ISubSequenceSparkMatcher):
             return len(SeqUtils.nt_search(x[1], subsequence)) > 1
 
         try:
-            conf = SparkConf().setAppName("ntsparksearch").setMaster("local[8]")
+            conf = SparkConf().setAppName("ntsparksearch").setMaster("spark://172.16.239.10:7077")
             # spark_session = SparkSession \
             #     .builder \
             #     .appName("ntsparksearch") \
